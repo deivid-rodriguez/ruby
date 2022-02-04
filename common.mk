154f36ad76cb67fecb1228ceae137f88ccec859a
David Rodríguez <deivid.rodriguez@riseup.net>
2022-02-04 12:10:54 +0100

    Workaround bundler issue

--- common.mk
+++ common.mk
@@ -1417,7 +1417,8 @@ yes-test-bundler-prepare: yes-test-bundler-precheck
 		-e 'ENV["BUNDLE_APP_CONFIG"] = File.expand_path(".bundle")' \
 		-e 'ENV["BUNDLE_PATH__SYSTEM"] = "true"' \
 		-e 'ENV["BUNDLE_WITHOUT"] = "lint doc"' \
-		-e 'load "spec/bundler/support/bundle.rb"' -- install --gemfile=tool/bundler/dev_gems.rb
+		-e 'load "spec/bundler/support/bundle.rb"' \
+		-e 'File.delete(".bundle/config")' -- install --gemfile=tool/bundler/dev_gems.rb
 	$(ACTIONS_ENDGROUP)
 
 RSPECOPTS =
